3d8f75faccb119deca55ba14b97cd1f5433ac6b1
THECoder12345324 <71527944+THECoder12345324@users.noreply.github.com>
2020-11-17 16:45:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup() {
     plinkos.push(new Plinko(j, 385, 10))
   }
 
+  //Creating the slider that will tell how many particles to drop
   slider = createSlider(10, 100, 20, 10);
   //plinko = new Plinko(width / 2, height / 2, 10);
 }
@@ -45,6 +46,8 @@ function draw() {
   Engine.update(engine);
 
   ground.display();
+  
+  //Displaying all of the divisions
   divisions[0].display();
   divisions[1].display();
   divisions[2].display();
@@ -53,30 +56,21 @@ function draw() {
   divisions[5].display();
   divisions[6].display();
 
+  //Displaying the Plinkos
   for (var j = 0; j < plinkos.length; j++) {
     plinkos[j].display();
   }
 
+  //Routinely adding more particles
   if (frameCount % 90 == 0) {
     for (var j = 0; j <= slider.value(); j++) {
       particles.push(new Particle(random(width / 2 - 50, width / 2 + 50), 0, 8))
     }
   }
 
+  //Displaying the particles
   for (var j = 0; j < particles.length; j++) {
     particles[j].display();
   }
   //plinko.display();
 }
-
-/*function keyPressed() {
-  if (keyCode == ENTER) {
-    for (var j = 0; j < particles.length; j++) {
-      particles[j].destroy();
-      particles.splice(j, 1);
-      particles.splice(j+1, 1);
-      particles.splice(j+2, 1);
-      particles.splice(j+3, 1);
-    }
-  }
-}*/
